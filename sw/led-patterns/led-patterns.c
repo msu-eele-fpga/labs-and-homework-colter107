0e7da2d3ccea876a4d0940b90a9c49482369017d
Colter VirtualBox <colter107@gmail.com>
2024-11-04 21:09:01 -0700

    Push to open lab 8 in windows devmem is currently not working

--- sw/led-patterns/led-patterns.c
+++ sw/led-patterns/led-patterns.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
-//#include <sys/mman.h> //for map
+#include <sys/mman.h> //for map
 #include <fcntl.h> //for file open flags
 #include <getopt.h>
-//#include <unistd.h> //for getting the page size
+#include <unistd.h> //for getting the page size
 
 void usage()
 {
@@ -13,21 +13,76 @@ void usage()
     fprintf(stderr, "led-patterns can be used to write patterns to the DE-10 Nano's LEDs using software.\n\n");
     fprintf(stderr, "Options:\n");
     fprintf(stderr, "-h                                     show this help message and exit \n");
-    fprintf(stderr, "-v                                     verbose: print LED pattern displayed as a binary string,\n");
-    fprintf(stderr, "                                       as well as how long each pattern is displayed.\n");
+    fprintf(stderr, "-v                                     verbose: print LED pattern displayed as a\n");
+    fprintf(stderr, "                                       binary string, as well as how long each \n");
+    fprintf(stderr, "                                       pattern is displayed\n");
     fprintf(stderr, "                                       IE: LED pattern = 11110000 Display time = 500ms\n");
-    fprintf(stderr, "-f [filename]                          file mode: reads from text file of given filename and writes the \n");
-    fprintf(stderr, "                                       corresponding patterns. Patterns and time will be given as shown:\n");
+    fprintf(stderr, "-f [filename]                          file mode: reads from text file of given \n");
+    fprintf(stderr, "                                       filename and writes the corresponding patterns.\n");
+    fprintf(stderr, "                                       Patterns and time will be given as shown:\n");
     fprintf(stderr, "                                           <pattern1> <time1> \n");
     fprintf(stderr, "                                           <pattern2> <time2>\n");
-    fprintf(stderr, "                                       where pattern is a hexadecimal value and time is an unsigned int in ms. \n");
+    fprintf(stderr, "                                       where pattern is a hexadecimal value and time is an \n");
+    fprintf(stderr, "                                       unsigned int in ms. \n");
     fprintf(stderr, "-p [pattern1, time1, pattern2 ...]     pattern mode: display each pattern for its respective time, \n");
-    fprintf(stderr, "                                       where pattern is a hexadecimal value and time is an unsigned int in ms. \n");
+    fprintf(stderr, "                                       where pattern is a hexadecimal value and time is an\n");
+    fprintf(stderr, "                                       unsigned int in ms. \n");
     fprintf(stderr, "                                       loop until exit command Ctrl-C is entered. \n");
     fprintf(stderr, "                                       if odd number of arguments, print error message and exit. \n");
     fprintf(stderr, "                                       MUST be enetered as last argument.\n");
     fprintf(stderr, "\n");
 }
+
+int devmem_read (int r_to_read) {
+
+    const size_t PAGE_SIZE = sysconf(_SC_PAGE_SIZE);
+    const char to_read = 0xff200000 + (4*r_to_read);
+    const uint32_t ADDRESS = to_read;
+
+    //Open the /dev/mem file, which is an image of the main system memory.
+    //Using synchronous write operations (O_SYNC) to ensure that the value
+    //is fully written to the underlying hardware before the write call returns
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    if (fd == -1)
+    {
+        fprintf(stderr, "failed to open /dev/mem. \n");
+        return 1;
+    }
+    
+    //mmap needs to map memory at page boundaries (address we're mapping must be aligned to a page).
+    //~(PAGE_SIZE -1) bitmask returns the closest page-alligned address that contains ADDRESS in the page.
+    //For 4096 byte page, (PAGE_SIZE -1 ) = 0xFFF; after flipping and converting to 32-bit we have a mask of -xFFFF_F000.
+    //AND'ing with this mask will make the 3 least significant nibbles of ADDRESS 0.
+    //This ensures the returned address is a multple of page size. Because 4096 - 0x1000, anything with three zeros for
+    //the least significant nibbles must be a multiple.
+
+    uint32_t page_aligned_addr = ADDRESS & ~(PAGE_SIZE - 1);
+
+    // Map a page of physical memory into virtual memory. 
+    // mor infos at mmap man page: https://www.man7.org/linux/man-pages/man2/mmap.2.html
+    uint32_t *page_virtual_addr = (uint32_t *)mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, page_aligned_addr);
+    if(page_virtual_addr == MAP_FAILED)
+    {
+        fprintf(stderr, "failed to map memory. \n");
+        return 1;
+    }
+
+    //The address we want to access is probably not page-aligned, so we need to find how far away from the page boundary it is.
+    //Using this offset, we can compute the virtual addres corresponding to the physical targed address (ADDRESS)
+    uint32_t offset_in_page =  ADDRESS & (PAGE_SIZE -1);
+
+    //Compute virtual address corrsponding to ADDRESS.
+    //Because virtual addresses are both uint32_t (integer) pointers, the pointer addition multiplies the address by the number
+    //of bytes needed to store a uint32_t. This is not what we want. So, we divide by this size (4 bytes) to make it return the
+    //right address. 
+
+    //We use volatile because the target virtual address could change outside the program.
+    //Volatile tells the compiler to not optimize accesses to this address.
+    volatile uint32_t *target_virtual_addr = page_virtual_addr + offset_in_page/sizeof(uint32_t*);
+
+    return *target_virtual_addr;  
+}
+    
 
 int main(int argc, char **argv)
 {   
@@ -36,36 +91,7 @@ int main(int argc, char **argv)
     bool patternMode;
     bool fileMode;
 
-    option = getopt(argc,argv,"hvpf:");
-    while(option != -1){
-        switch(option){
-            case 'h':
-                usage();
-                break;
-            case 'v':
-                printf("Verbose\n");
-                verbose = true;
-                break;
-            case 'p':
-                printf("Pattern\n");
-                patternMode = true;
-                break;
-            case 'f':
-                printf("File\n");
-                fileMode = true;
-                printf("File Given: %s\n", optarg);
-                break;
-            case '?':
-                printf("Unknown argument %c\n", optopt);
-                break;
-        }
-        option = getopt(argc,argv,"hvpf");
-    }
-    
-    
-    
-    /*//size of a page of memory in the system (typically 4096 bytes)
-    const size_t PAGE_SIZE = sysconf(_SC_PAGE_SIZE);
+
     if (argc == 1)
     {
         //No arguments were given, so just print usage text and exit
@@ -75,7 +101,40 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    //If the VALUE argument was given, we'll perform a write operation
+    option = getopt(argc,argv,"hvpf:");
+    while(option != -1){
+        switch(option){
+            case 'h':
+                usage();
+                break;
+            case 'v':
+                verbose = true;
+                break;
+            case 'p':
+                patternMode = true;
+                break;
+            case 'f':
+                printf("File\n");
+                fileMode = true;
+                break;
+            case '?':
+                printf("Unknown argument %c\n", optopt);
+                break;
+        }
+        option = getopt(argc,argv,"hvpf");
+        if(fileMode && patternMode){
+            printf("ERROR: both file mode and pattern mode given\n");
+            printf("Exiting program... \n");
+            return 1;
+        }
+    }
+
+    printf("%d\n",devmem_read('0'));
+    
+
+    
+
+    /*//If the VALUE argument was given, we'll perform a write operation
     bool is_write = (argc ==3 ) ? true : false;
 
     const uint32_t ADDRESS = strtoul(argv[1], NULL, 0);
