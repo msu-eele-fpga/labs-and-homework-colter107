c6c7d276263c8fdcaa7cce491d22c7396818e552
colter107 <101423601+colter107@users.noreply.github.com>
2024-11-14 23:38:09 -0700

    Added working PWM controller. No range checking for PWM period yet.

--- hdl/PWM_Controller/PWM_Controller.vhd
+++ hdl/PWM_Controller/PWM_Controller.vhd
@@ -30,10 +30,25 @@ constant PERIOD_WHOLE : natural := 6;
 constant DUTY_CYCLE_WHOLE : natural := 1;
 
 --Number of bits used to store the system clock frequency as unsinged int
-constant system_clock_freq_num_bits : natural := natural(ceil(log2(real(1 sec / CLK_PERIOD))));
+constant system_clock_freq_num_bits : natural := natural(ceil(log2(real(1 sec / (1000 * CLK_PERIOD)))));
 
  --System clock frequency
-constant system_clock_freq : unsigned(system_clock_freq_num_bits - 1 downto 0) := TO_UNSIGNED((1 sec / CLK_PERIOD), system_clock_freq_num_bits);
+constant system_clock_freq : unsigned(system_clock_freq_num_bits - 1 downto 0) := TO_UNSIGNED((1 sec / (1000 * CLK_PERIOD)), system_clock_freq_num_bits);
+
+--System clock frequency (cycles/sec) * (1 sec / 1000 ms) = cycles / ms (no fractional bits)
+
+--Clock frequency * PWM period 
+--      cycles/ms * seconds = clock cycles/1000 (good)
+--      total bits = system_clock_freq_num_bits + period_width
+--      whole bits = system_clock_freq_num_bits + period_whole
+--      assign total_bits downto period_fractional
+--
+--PWM cycles * Duty cycle
+--      cycles/1000 * fraction of cycles to switch at (good)
+--      total bits = system_clock_freq_num_bits + period_width + duty_cycle_width
+--      whole bits = system_clock_freq_num_bits + period_whole + duty_cycle whole
+--      assign total_bits downto (period_fractional + duty_cycle_fractional)
+
 
 --CONSTANTS FOR PWM PERIOD------------------------------------------------------------------------------------------------------------------------
 --Number of bits needed to represent system clock frequency * period
@@ -63,55 +78,44 @@ constant duty_cycle_clock_cycles_num_bits_total : natural := PERIOD_WIDTH + DUTY
 
 --Number of clock cycles in one period
 --This has fractional bits, but we can't have fractional clock cycles
-signal duty_cycle_clk_cycles_full : unsigned(period_clock_cycles_num_bits_total - 1 downto 0);
+signal duty_cycle_clk_cycles_full : unsigned(duty_cycle_clock_cycles_num_bits_total - 1 downto 0);
 
 --Number of whole clock cycles in one base period
 --No fractional bits :D
-signal duty_cycle_clk_cycles : unsigned(period_clock_cycles_num_bits_whole - 1 downto 0);
+signal duty_cycle_clk_cycles : unsigned(duty_cycle_clock_cycles_num_bits_whole - 1 downto 0);
 
-signal counter : unsigned (PERIOD_WIDTH downto 0);
-signal pwm : STD_ULOGIC;
+signal counter : unsigned (PERIOD_WIDTH downto 0) := (others => '0');
+signal pwm : STD_ULOGIC := '0';
 
 
 begin
+
 --Set number of clock cycles in period
 period_clk_cycles_full <= system_clock_freq * period;
 period_clk_cycles <= period_clk_cycles_full(period_clock_cycles_num_bits_total - 1 downto PERIOD_FRACTIONAL);
 
 --Set number of clock cycles in duty cycle
 duty_cycle_clk_cycles_full <= period_clk_cycles_full * duty_cycle;
-duty_cycle_clk_cycles <= period_clk_cycles_full(duty_cycle_clock_cycles_num_bits_total - 1 downto (PERIOD_FRACTIONAL + DUTY_CYCLE_FRACTIONAL));
+duty_cycle_clk_cycles <= duty_cycle_clk_cycles_full(duty_cycle_clock_cycles_num_bits_total - 1 downto PERIOD_FRACTIONAL + DUTY_CYCLE_FRACTIONAL);
 
-adjust_range: process(duty_cycle_clk_cycles)
-begin
-    if (to_integer(duty_cycle_clk_cycles) > 1) then
-        duty_cycle_clk_cycles <= period_clk_cycles;
-    elsif (to_integer(duty_cycle_clk_cycles) <= 0) then 
-        duty_cycle_clk_cycles <= (others => '0');
-    else
-        duty_cycle_clk_cycles <= duty_cycle_clk_cycles;
-    end if;
-end process adjust_range;
-    
 pulse_on : process(clk,rst)
 begin
     if(rst = '1') then
         counter <= (others => '0');
-    else
-        if(rising_edge(clk))then
-            --If counter reaches period, turn on PWM signal, reset timer
-            if(counter >= period_clk_cycles) then
-                pwm <= '1';
-                counter <= (others => '0');
-            --If counter is between end of duty cycle and period, turn off PWM signal
-            elsif counter >= duty_cycle_clk_cycles  then
-                pwm <= '0';
-            --Otherwise, the counter is between the start of the period and the end of duty cycle, and PWM should be on
-            else
-                pwm <= '1';
-            end if;
-        output <= pwm;
+    elsif(rising_edge(clk))then
+        counter <= counter + 1;
+        --If counter reaches period, turn on PWM signal, reset timer
+        if counter >= period_clk_cycles then
+            pwm <= '1';
+            counter <= (others => '0');
+        --If counter is between end of duty cycle and period, turn off PWM signal
+        elsif counter >= duty_cycle_clk_cycles  then
+            pwm <= '0';
+        --Otherwise, the counter is between the start of the period and the end of duty cycle, and PWM should be on
+        else
+            pwm <= '1';
         end if;
+        output <= pwm;
     end if;
 end process pulse_on;
 end architecture;
--- sim/PWM controller/PWM_Controller_TB.vhd
+++ sim/PWM controller/PWM_Controller_TB.vhd
@@ -11,9 +11,47 @@ end entity;
 architecture testbench of PWM_controller_TB is
     signal clk_tb               : std_ulogic := '0';
     signal rst_tb               : std_ulogic := '0';
-    signal period_tb            : std_ulogic_vector (26 downto 0) := "00000000000000000000000000";
-    signal duty_cycle_tb        : std_ulogic_vector (14 downto 0) := "000000000000000";
-    
+--                                                           wwwwwwfffffffffffffffffffff
+    signal period_tb            : unsigned (26 downto 0) := "000000100000000000000000000"; --500 us
+--                                                           wffffffffffffff
+    signal duty_cycle_tb        : unsigned (14 downto 0) := "001000000000000"; --1/4
+    signal out_tb               : std_ulogic;
+
+component pwm_controller is
+        generic (
+        CLK_PERIOD : time := 20 ns
+        );
+        port (
+            clk : in std_logic;
+            rst : in std_logic;
+            -- PWM repetition period in milliseconds;
+            -- datatype (W.F) is individually assigned
+            period : in unsigned(27 - 1 downto 0);
+            -- PWM duty cycle between [0 1]; out-of-range values are hard-limited
+            -- datatype (W.F) is individually assigned
+            duty_cycle : in unsigned(15 - 1 downto 0);
+            output : out std_logic
+        );
+end component pwm_controller;
 begin
 
+DUT1: pwm_controller
+        port map (
+            clk => clk_tb,
+            rst => rst_tb,
+            period => period_tb,
+            duty_cycle => duty_cycle_tb,
+            output => out_tb
+        );
+
+clk_tb <= not clk_tb after CLK_PERIOD /2;
+
+stimuli_and_checker : process is 
+begin
+    rst_tb <= '1';
+    wait_for_clock_edges(clk_tb, 10);
+    rst_tb <= '0';
+    wait_for_clock_edges(clk_tb, 30000000);
+
+end process stimuli_and_checker;
 end architecture; 
